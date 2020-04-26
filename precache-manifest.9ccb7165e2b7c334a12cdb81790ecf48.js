1f45f3e7af6f6841245f81392261ce8d57feea44
phuctt4 <phuctt4@vng.com.vn>
2020-04-26 23:04:55 +0700

    Updates

---
+++ precache-manifest.9ccb7165e2b7c334a12cdb81790ecf48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a826e51dd3b4e97e5cb0fb1483a4d1a5",
+    "url": "/covid-map/index.html"
+  },
+  {
+    "revision": "6187799203187c51d69a",
+    "url": "/covid-map/static/css/main.9ef9d325.chunk.css"
+  },
+  {
+    "revision": "f895de23a9593e0a3532",
+    "url": "/covid-map/static/js/2.be038498.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/covid-map/static/js/2.be038498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6187799203187c51d69a",
+    "url": "/covid-map/static/js/main.0fa1a6ad.chunk.js"
+  },
+  {
+    "revision": "7e650903f8bca7e285b7",
+    "url": "/covid-map/static/js/runtime-main.182fed69.js"
+  },
+  {
+    "revision": "c57ab61bcaf2279c296a51ffa0d4afaf",
+    "url": "/covid-map/static/media/pause.c57ab61b.svg"
+  },
+  {
+    "revision": "7e2f9a3f4a239fb2587563ac7dd13d20",
+    "url": "/covid-map/static/media/play.7e2f9a3f.svg"
+  }
+]);
